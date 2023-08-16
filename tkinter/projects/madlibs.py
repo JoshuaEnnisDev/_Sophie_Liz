12e032e657f1e4ae9fe7f20ae7e14fefe3924d23
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-08-16 16:00:05 -0500

    commit

--- tkinter/projects/madlibs.py
+++ tkinter/projects/madlibs.py
@@ -8,14 +8,14 @@ window.geometry("500x500")
 
 verb_label = ttk.Label(window, text="Verb")
 verb_entry = ttk.Entry(window)
-verb_label.pack()
-verb_entry.pack()
+verb_label.pack(side='left')
+verb_entry.pack(side='left')
 story = ttk.Label(window)
 
 
 def go():
     story['text'] = f'''
-                I was {verb_entry.get()}ing to the store when/n
+                I was {verb_entry.get()}ing to the store when\n
                 a man came up to me and {verb_entry.get()}
             '''
     story.pack()
--- tkinter/projects/number_guesser.py
+++ tkinter/projects/number_guesser.py
@@ -1,8 +1,33 @@
 import tkinter as tk
+from random import randint
+
+cmp_guess = randint(1, 100)
+num_guesses = 0
 
 
 def guess():
-    pass
+    try:
+        entry_text.get()
+    except tk.TclError:
+        label_text.set("Enter a number silly")
+        entry_text.set('')
+        return
+
+    global num_guesses
+    num_guesses += 1
+    button_text.set(f"Guesses {num_guesses}")
+    # get user guess
+    user_guess = entry_text.get()
+
+    # clear entry IntVar
+    entry_text.set("")
+
+    if user_guess > cmp_guess:
+        label_text.set(f"{user_guess} was too high.")
+    elif user_guess < cmp_guess:
+        label_text.set(f"{user_guess} was too low.")
+    else:
+        label_text.set("Correct!")
 
 
 # window setup
@@ -15,11 +40,19 @@ label_text = tk.StringVar(value="Enter a number between 1 and 100")
 label = tk.Label(window, textvariable=label_text, font=("Courier", 14))
 label.pack(pady=10)
 
-entry = tk.Entry(window, font=("Courier", 14))
+entry_text = tk.IntVar(value='')
+entry = tk.Entry(window, font=("Courier", 14), textvariable=entry_text)
 entry.pack(pady=10)
 
 button_text = tk.StringVar(value='guess')
-button = tk.Button(window, textvariable=button_text, command=guess)
+button = tk.Button(
+    window, 
+    textvariable=button_text,
+    command=guess,
+    font=("Courier", 14, 'bold'),
+    foreground='green'
+)
+
 button.pack(pady=10)
 
 # make sure this is the last line
